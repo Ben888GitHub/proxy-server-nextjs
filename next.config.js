76051f22661aa17f599d3df2dc7eea5950db274c
Benedict <benedictryan80@gmail.com>
2022-04-12 01:08:37 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = () => {
+	const rewrites = () => {
+		return [
+			{
+				source: '/cats',
+				destination: 'https://meowfacts.herokuapp.com'
+			},
+			{ source: '/ducks', destination: 'https://random-d.uk/api/random' }
+		];
+	};
+	// Rewrites allow you to map an Incoming Request Path to a different destination path.
+	return { rewrites };
+};
